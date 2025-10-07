ddda1116a28b49b894c8019063cd97f2b586ecc0
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-10-07 10:54:05 +0600

    Solved carrot problem

---
+++ carrots.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int numberOfPeople, problemSolved;
+    cin >> numberOfPeople >> problemSolved;
+
+    for (int i = 0; i < numberOfPeople; i++)
+    {
+        string inputString;
+        cin >> inputString;
+    }
+
+    cout << problemSolved;
+
+    return 0;
+}
