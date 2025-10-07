b452ca2d2d6e36d2173884a34d0a5cb42926ad0a
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-10-07 10:41:28 +0600

    Solved R2 problem

---
+++ r_2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int firstNumber, finalNumber;
+    cin >> firstNumber >> finalNumber;
+
+    int secondNumber = (2 * finalNumber) - firstNumber;
+    cout << secondNumber;
+
+    return 0;
+}
