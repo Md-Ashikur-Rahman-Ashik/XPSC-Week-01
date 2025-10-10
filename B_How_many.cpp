cd74f8895525cbb726d27346814c792523d2705f
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-10-10 18:23:25 +0600

    Couldn't solve how many problem

---
+++ B_How_many.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int maximumSum, maximumMultiple;
+    cin >> maximumSum >> maximumMultiple;
+
+    int maxLimit = max(maximumSum, maximumMultiple);
+
+    int count = 0;
+
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    
+
+    cout << count;
+
+    return 0;
+}
